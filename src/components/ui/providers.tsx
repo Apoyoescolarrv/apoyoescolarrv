1e6472abcae8d2df0f074cc7c7396c8917b07c4a
Agustin Garcia <apoyoescolarrv@gmail.com>
2024-12-17 11:16:42 -0300

    chore: store reqs in cache

--- src/components/ui/providers.tsx
+++ src/components/ui/providers.tsx
@@ -7,6 +7,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
     defaultOptions: {
       queries: {
         retry: 5,
+        staleTime: 1000 * 60 * 5, // 5 minutos
+        gcTime: 1000 * 60 * 10, // 10 minutos
       },
     },
   });
